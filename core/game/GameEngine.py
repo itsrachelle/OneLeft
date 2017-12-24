9a3f2e6f394ace6b5da2382a29be87cde7884d2f
Rachel Ramos <rachelle.r.chan@gmail.com>
2017-12-24 16:29:26 -0600

    Added cards used count, card in pile count, deck size (default 108), and get players hand count method

--- core/game/GameEngine.py
+++ core/game/GameEngine.py
@@ -11,6 +11,7 @@ from core.game.ColorType import ColorType
 INITIAL_HAND_SIZE: int = 7
 MIN_REQUIRED_PLAYERS: int = 2
 MAX_REQUIRED_PLAYERS: int = 10
+DECK_SIZE: int = 108
 
 
 class GameEngine:
@@ -21,8 +22,10 @@ class GameEngine:
         self._players: List[IPlayer] = players
         self._roundsPerMatch: int = rounds_per_match
         self._deck: List[Card] = GameEngineHelper.create_game_deck()
+        self._amount_of_cards_used_in_round: int = 0
         self._top_card_in_pile: Card = None
         self._card_played: Card = None
+        self._card_pile: List[Card] = []
 
     def __get_game_players(self, players: List[IPlayer]) -> List[GamePlayer]:
         game_players = []
@@ -36,12 +39,15 @@ class GameEngine:
         drawn_cards = []
         for n in range(draw_count):
             drawn_cards.append(self._deck.pop(0))
+        self._amount_of_cards_used_in_round += drawn_cards.count()
         return drawn_cards
 
     def __create_player_game_helper(self, active_player: GamePlayer, last_card_played: Card):
         hand = active_player.hand
         last_card_played = last_card_played
-        card_pile = []
+        # we should check before we append
+        self._card_pile.append(last_card_played)
+        card_pile = self._card_pile
         deck_count = len(self._deck)
         opp_hand_count = []
         return PlayerGameHelper(hand, last_card_played, card_pile, deck_count, opp_hand_count)
@@ -82,11 +88,17 @@ class GameEngine:
         # This should work now that I added a setter property on GamePlayer.hand
         game_player.hand.extend(card_drawn)
 
+    def get_players_hand_counts(self, players: List[GamePlayer]) -> int:
+        hand_count = 0
+        for player in players:
+            hand_count += player.hand.count()
+        return hand_count
+
     def start(self):
         current_round: int = 0
 
         while current_round <= self._roundsPerMatch:
-            # At the beginning of every round, get a fresh list of players and resent round variables.
+            # At the beginning of every round, get a fresh list of players and reset round variables.
             round_players = self.__get_game_players(self._players)
             self._deck = GameEngineHelper.create_game_deck()
             current_round += 1
@@ -96,20 +108,29 @@ class GameEngine:
 
             # Keep playing the same round until a winner is chosen.
             while not round_won:
+
                 active_player: GamePlayer = None
                 # No matter what the player (easy,hard,etc. we will play the first card from the deck
                 last_card_played = self.__draw_cards(1)
-
                 # Main game loop starts here, loop through each player until a player has 0 cards.
                 for game_player in round_players:
-                    active_player = game_player.player
-                    active_player_game_helper = self.__create_player_game_helper(game_player, last_card_played)
-                    game_player.player.set_game_helper(active_player_game_helper)
-                    # Check for skip before anything else is done (early exit) <- this is done in legal responses
 
+                    active_player = game_player.player
+
+                    # create PlayerGameHelper
+                    active_player_game_helper = self.__create_player_game_helper(game_player, last_card_played)
+                    # PlayerGameHelper is only for this turn for this player
+                    game_player.player.set_game_helper(active_player_game_helper)
                     current_game_helper = game_player.player.get_game_helper()
 
+                    cards_used_from_deck = self._amount_of_cards_used_in_round + self.get_players_hand_counts(
+                        round_players)
+                    if cards_used_from_deck >= DECK_SIZE:
+                        # reshuffle logic here
+                        pass
+
                     player_action = game_player.player.take_turn()
+
                     self._card_played = player_action.card
 
                     # check what was played is legal and from their hand AND they aren't skipping
@@ -122,6 +143,8 @@ class GameEngine:
                             self.draw_card_to_hand(game_player, 1)
                             continue
                         else:
+                            # This card can be rightfully added to the pile
+                            self._card_pile.append(last_card_played)
                             # IT IS LEGAL remove card from hand (except for skip)
                             # We need logic to handle actions here too (Draw two, reverse, skip, wild +4, wild
                             pass
